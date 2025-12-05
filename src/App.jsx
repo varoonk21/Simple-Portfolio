5e81c860fc230fc9d07a9d9bd4f76ee188cdb71f
Varoon Kumar <varoonk21@gmail.com>
2025-12-06 02:14:41 +0500

    resume updated

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   const downloadResume = () => {
     const a = document.createElement("a");
-    a.href = "/resume.pdf";
+    a.href = "/varoon_resume.pdf";
     a.download = "Varoon_Kumar_Resume.pdf";
     a.click();
   };
